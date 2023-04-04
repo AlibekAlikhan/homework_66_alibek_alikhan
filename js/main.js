4b5651206e006d634793e530ce255d0e5ab80b1c
Alibek Alikhan <alialikhan0520@gmail.com>
2023-04-04 19:05:07 +0600

    Изменил порядок вывода в 1 задании

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-console.log(document.getElementById('string-1').innerText)
-console.log(document.getElementById('string-2').innerText)
 console.log(document.getElementById('string-3').innerText)
-console.log(document.getElementById('string-4').innerText)
 console.log(document.getElementById('string-5').innerText)
+console.log(document.getElementById('string-2').innerText)
 console.log(document.getElementById('string-6').innerText)
+console.log(document.getElementById('string-4').innerText)
+console.log(document.getElementById('string-1').innerText)
 
 const element = document.getElementsByClassName('element')
 len = 0
