9e76c3676ff5c45a35ffab20711d88aace8cc044
Alibek Alikhan <alialikhan0520@gmail.com>
2023-04-04 14:56:07 +0600

    Сделал 2 задание

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ console.log(document.getElementById('string-3').innerText)
 console.log(document.getElementById('string-4').innerText)
 console.log(document.getElementById('string-5').innerText)
 console.log(document.getElementById('string-6').innerText)
+
+const element = document.getElementsByClassName('element')
+len = 0
+for(i of element){
+  len++
+    if(len < 4){
+        i.style.color = 'red'
+    }
+    else{
+        i.style.color = 'green'
+    }
+}
+
